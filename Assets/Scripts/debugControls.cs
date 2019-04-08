05d190ed0f97740d706cbca69c2545d5237e2042
Alexis <alexis.morin.mail@gmail.com>
2019-04-07 22:25:35 -0400

    added debug controls

---
+++ Assets/Scripts/debugControls.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class debugControls : MonoBehaviour {
+    void Update () {
+        if (Input.GetKeyDown (KeyCode.F10)) {
+            Scene scene = SceneManager.GetActiveScene ();
+            SceneManager.LoadScene (scene.name);
+        }
+
+        if (Input.GetKeyUp (KeyCode.Escape)) {
+            Application.Quit ();
+        }
+
+    }
+}
